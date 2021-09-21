30119ce005586e82e8b1a98c612debde0c40185e
rarahata <rei.arahata@gmail.com>
2021-09-21 14:32:25 +0100

    add format date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
+function formatTime(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day = days[date.getDay()];
+    return `${hours}:${minutes}`;
+}
+
+
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day = days[date.getDay()];
+    let months = ["January","February","March","April","May","June","July","August","Septemper","Octpber","November","December"];
+    let month = months[date.getMonth()];
+    return `${day}, ${month}`;
+}
+
+
 function showTemperature(response){
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = response.data.name;
@@ -9,6 +29,10 @@ function showTemperature(response){
     humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt*1000);
+    let timeElement = document.querySelector("#time");
+    timeElement.innerHTML = formatTime(response.data.dt*1000);
 }
 
 let apiKey ="7796ed76d4738ed90e39d5875eb78f75";
